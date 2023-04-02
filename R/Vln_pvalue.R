eb5254454ca977e2b95456e30830dae92eda9474
frolova <alex.frolov@dzne.de>
2023-04-02 12:20:10 +0200

    New Features:
    - For Vln_pvalue you can now choose which slot to use and if you plot the values in log scale
    
    - For abs_by_cond it is possible to show total cell numbers in addition to relative stacked bar plot

--- R/Vln_pvalue.R
+++ R/Vln_pvalue.R
@@ -4,11 +4,24 @@
 #' @param gene Gene for which the significance test was performed
 #' @param Condition name of metadata column to test for DEG
 #' @param DEGs List of Differentially Expressed genes with their respective p values (output from FindMarkers)
+#' @param split column name in meta used for split.by in Violin plot
+#' @param assay which Seurat assay to use
+#' @param slot which slot in the assay to use, possible: "counts", "data" or "scale.data"
+#' @param log are the values in Violin plot represented in a logarithmic scale
 #' @export
-Vln_pvalue<-function(SeuratObject,gene,Condition,DEGs){
+Vln_pvalue<-function(SeuratObject,gene,Condition,DEGs,split="filler",assay="RNA",slot="counts",log=T){
 
+  dataset.markers<-list()
   ###create test object, with positions for p values in plot
-  vln_df = data.frame(expr = SeuratObject[["RNA"]]@data[gene,], cluster = Idents(SeuratObject),Condition = SeuratObject[[Condition]])
+  count.data <- GetAssayData(object =SeuratObject[[assay]], slot = slot)
+
+  if(slot=="counts"){
+  count.data <- as.matrix(x = count.data + 1)
+  }
+  if(log==T){
+    count.data<-log(count.data,10)
+  }
+  vln_df = data.frame(expr = count.data[gene,], cluster = Idents(SeuratObject),Condition = SeuratObject[[Condition]])
   names(vln_df)[3]<-"Condition"
   # ggplot(vln_df,aes(x=expr))+geom_histogram(aes(color=genotype),fill="white")+theme_prism()
 
@@ -40,7 +53,7 @@ Vln_pvalue<-function(SeuratObject,gene,Condition,DEGs){
 
 
     ##because VlnPlot sets the fill aesthetics globally, the variable for fill needs to be present, even if not used.
-    vln_signif$split<-"filler"
+    vln_signif$split<-split
 
     dataset.markers$selected_genes[[gene]]<-vln_signif
 
